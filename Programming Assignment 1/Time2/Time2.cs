0a7cc787fd6bf1b5ff95f5ebd482723d6cd76d9a
Austin Hill <austin.hill42@gmail.com>
2017-06-27 18:20:38 -0500

    First Commit

---
+++ Programming Assignment 1/Time2/Time2.cs
@@ -0,0 +1,12 @@
+﻿// Time2 class declaration with overloaded constructors
+using System;   // for class ArgumentOutOfRangeException
+
+public class Time2
+{
+    private int hour;       // 0 - 23
+    private int minute;     // 0 - 59
+    private int second;     // 0 - 59
+
+
+}
+
